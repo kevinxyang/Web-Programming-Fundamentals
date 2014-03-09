fe320abde7d1e53c4bd22f013c3f4c2403fc738e
kevinxyang <kyang@fullsail.edu>
2014-03-09 15:23:39 -0400

    Finished problem 1.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,7 +7,10 @@
 // Dog Years
 // =========
 
+//Declared and defined Sparky's real age.
 var humanAge = 3;
+//Declared Sparky's dog age and defined it to be 7 times his real age.
 var dogAge = humanAge * 7;
 
-console.log(dogAge);
+//Printed Sparky's real and dog age to the console.
+console.log("Sparky is" + " " + humanAge + " " + "which is" + " " + dogAge + " " + "in dog years.");
