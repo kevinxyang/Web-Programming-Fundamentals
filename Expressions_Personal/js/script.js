eca977809a67eb2d2c122812d5eb942183f727d8
kevinxyang <kyang@fullsail.edu>
2014-03-11 14:40:52 -0400

    Changed food tracker to fitness tracker.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+// Personal - Food Tracker
+// =======================
+
+var numberOfBreakfastItems = prompt("How many items did you have for breakfast?");
+var breakfastLength = 1 - numberOfBreakfastItems;
+var breakfastItems = Array(breakfastLength);
+console.log(numberOfBreakfastItems);
