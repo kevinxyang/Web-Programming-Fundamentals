0adf4c2ecdcdcfb591d7d06f5357c9a790546422
kevinxyang <kyang@fullsail.edu>
2014-03-11 18:41:42 -0400

    Commented the code for the personal subproject, still need to finalize output.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,56 +2,81 @@
 // =======================
 
 // Givens
+// ======
 
 // Bench Press
+// Declared variables for the amount bench pressed, number of sets of bench
+// presses, and the total number of reps in each set.
 var benchpressWeight = prompt("How much weight did you bench press today?", 
     "Enter the weight in lbs.");
 var benchpressSets = prompt("# of sets of Bench Presses",
     "Enter the total number of sets here.");
 var benchpressReps = prompt("# of Bench Presses per set",
-	"Enter the total number of reps in each set here.");
+    "Enter the total number of reps in each set here.");
 
 // Squat
+// Declared variables for the amount squatted, number of sets of squats, and the
+// total number of reps in each set.
 var squatWeight = prompt("How much weight did you squat today?", 
     "Enter the weight in lbs.");
 var squatSets = prompt("# of sets of Squats",
     "Enter the total number of sets here.");
 var squatReps = prompt("# of Squats per set",
-	"Enter the total number of reps in each set here.");
+    "Enter the total number of reps in each set here.");
 
 // Deadlift
+// Declared variables for the amount deadlifted, number of sets of deadlifts, 
+// and the total number of reps in each set.
 var deadliftWeight = prompt("How much weight did you deadlift today?", 
     "Enter the weight in lbs.");
 var deadliftSets = prompt("# of sets of Deadlifts",
     "Enter the total number of sets here.");
 var deadliftReps = prompt("# of Deadlifts per set",
-	"Enter the total number of reps in each set here.");
+    "Enter the total number of reps in each set here.");
 
 // Overhead Press
+// Declared variables for the amount overhead pressed, number of sets of
+// overhead presses, and the total number of reps in each set.
 var overheadpressWeight = prompt("How much weight did you overhead press today?"
     , "Enter the weight in lbs.");
 var overheadpressSets = prompt("# of sets of Overhead Presses",
     "Enter the total number of sets here.");
 var overheadpressReps = prompt("# of Overhead Presses per set",
-	"Enter the total number of reps in each set here.");
+    "Enter the total number of reps in each set here.");
+
+
 
 // Calculations based on givens
+// ============================
 
+// Takes the user's input for the bench press, and calculates the total amount
+// of weight bench pressed.
 var totalBench = benchpressWeight * benchpressSets * benchpressReps;
+// Takes the user's input for the bench press, and calculates the total amount
+// of weight squatted.
 var totalSquat = squatWeight * squatSets * squatReps;
+// Takes the user's input for the bench press, and calculates the total amount
+// of weight deadlifted.
 var totalDeadlift = deadliftWeight * deadliftSets * deadliftReps;
+// Takes the user's input for the bench press, and calculates the total amount
+// of weight overhead pressed.
 var totalOverheadpress = overheadpressWeight * overheadpressSets
     * overheadpressReps;
 
+// Calculates the total amount lifted today.
 var totalWeight = totalBench + totalSquat + totalDeadlift + totalOverheadpress;
+// Calculates the total number of sets performed today.
 var totalSets = Number(benchpressSets) + Number(squatSets)
     + Number(deadliftSets) + Number(overheadpressSets);
+// Calculates the total number of reps performed today.
 var totalReps = Number(benchpressReps) * Number(benchpressSets)
     + Number(squatReps) * Number(squatSets) + Number(deadliftReps)
     * Number(deadliftSets) + Number(overheadpressReps)
     * Number(overheadpressSets);
 
+// Calculates the average weight lifted per set.
 var averageWeightPerSet = totalWeight / totalSets;
+// Calculates the average weight lifted per rep.
 var averageWeightPerRep = totalWeight / totalReps;
 
 console.log(totalBench);
@@ -62,3 +87,4 @@ console.log(totalWeight);
 console.log(averageWeightPerSet);
 console.log(averageWeightPerRep);
 
+// End
