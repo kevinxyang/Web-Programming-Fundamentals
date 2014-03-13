87698382a350885b4e924b8d7f334d912a93509d
kevinxyang <kyang@fullsail.edu>
2014-03-13 14:32:02 -0400

    Wrote code and printed the result to console.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+// Wacky - How many licks...?
+// ==========================
+
+// Givens
+// ======
+
+var time = prompt("How long (in minutes) did it take you to reach the center of the Tootsie Pop?");
+var numberOfLicks = prompt("How many licks did it take you to reach the center of the Tootsie Pop?");
+var flavor = prompt("What flavor was the Tootsie Pop?");
+
+
+
+// Calculations based on givens
+// ============================
+
+var licksPerSecond = numberOfLicks / (time * 60);
+
+
+
+// Output
+console.log("It took you " + numberOfLicks + " licks and an average of " 
+    + licksPerSecond + " licks per second to get to the center of a " + flavor 
+    + " Tootsie Pop.");
