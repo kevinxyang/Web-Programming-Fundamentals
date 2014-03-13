196d8975d2e97f68d8f6e5192ecaab293912c5fb
kevinxyang <kyang@fullsail.edu>
2014-03-13 14:10:40 -0400

    Fixed the output and commented the code.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,21 +4,35 @@
 // Givens
 // ======
 
-var googleUsers = prompt("How many users were directed to your site through Google?");
+// Prompts the user for the total number of users both first time and returning
+// that used Google and stores them inside two variables.
+var googleUsers = prompt("How many users were directed to your site via Google?");
 var googleReUsers = prompt("How many of these users have been to your site before?");
 
-var bingUsers = prompt("How many users were directed to your site through Bing?");
+// Prompts the user for the total number of users both first time and returning
+// that used Bing and stores them inside two variables.
+var bingUsers = prompt("How many users were directed to your site via Bing?");
 var bingReUsers = prompt("How many of these users have been to your site before?");
 
-var yahooUsers = prompt("How many users were directed to your site through Yahoo?");
+// Prompts the user for the total number of users both first time and returning
+// that used Yahoo! and stores them inside two variables.
+var yahooUsers = prompt("How many users were directed to your site via Yahoo?");
 var yahooReUsers = prompt("How many of these users have been to your site before?");
 
-var dogpileUsers = prompt("How many users were directed to your site through Dogpile?");
+// Prompts the user for the total number of users both first time and returning
+// that used Dogpile and stores them inside two variables.
+var dogpileUsers = prompt("How many users were directed to your site via Dogpile?");
 var dogpileReUsers = prompt("How many of these users have been to your site before?");
 
-var otherUsers = prompt("How many users were directed to your site through some other site?");
+// Prompts the user for the total number of users both first time and returning
+// that used some other search engine and stores them inside two variables.
+var otherUsers = prompt("How many users were directed to your site via some other search engine?");
 var otherReUsers = prompt("How many of these users have been to your site before?");
 
+// Take the variables above, and stores them inside a multidimensional array to
+// keep them organized and easy to access. The outermost array holds the type
+// of search engine, while the innermost array holds the user data of each
+// individual search engine.
 var siteTracker = Array();
 
 siteTracker[0] = Array();
@@ -51,11 +65,16 @@ siteTracker[4][2] = Number(otherReUsers);
 // Calculations based on givens
 // ============================
 
+// Using the user data from each search engine, we can calculate the total
+// number of first time users.
 totalFirstTimeUsers = siteTracker[0][1] - siteTracker[0][2] + siteTracker[1][1]
     - siteTracker[1][2] + siteTracker[2][1] - siteTracker[2][2] 
     + siteTracker[3][1] - siteTracker[3][2] + siteTracker[4][1] 
     - siteTracker[4][2];
 
+// By taking the user data from each search engine and dividing it by the total
+// number of first time users, we can calculate what percent of first time users
+// used which search engine.
 percentageGoogle = (siteTracker[0][1] - siteTracker[0][2]) / totalFirstTimeUsers * 100;
 percentageBing = (siteTracker[1][1] - siteTracker[1][2]) / totalFirstTimeUsers * 100;
 percentageYahoo = (siteTracker[2][1] - siteTracker[2][2]) / totalFirstTimeUsers * 100;
@@ -64,12 +83,13 @@ percentageOther = (siteTracker[4][1] - siteTracker[4][2]) / totalFirstTimeUsers 
 
 
 
-// Output
-console.log(totalFirstTimeUsers);
-console.log(percentageGoogle);
-console.log(percentageBing);
-console.log(percentageYahoo);
-console.log(percentageDogpile);
-console.log(percentageOther);
+// Prints the total number of first time users, along with what percentage of
+//first time users used Google, Bing, Yahoo!, Dogpile, or another search engine.
+console.log(totalFirstTimeUsers + " users visited your site for the first time.");
+console.log(Math.floor(percentageGoogle) + "% of them used Google.");
+console.log(Math.floor(percentageBing) + "% of them used Bing.");
+console.log(Math.floor(percentageYahoo) + "% of them used Yahoo!.");
+console.log(Math.floor(percentageDogpile) + "% of them used Dogpile.");
+console.log(Math.floor(percentageOther) + "% of them used some other search engine.");
 
 // End
