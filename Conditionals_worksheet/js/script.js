449d3f471beecb51744c70c5122e72d8ca308e55
kevinxyang <kyang@fullsail.edu>
2014-03-17 17:54:45 -0400

    Added the Gas calculator.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,17 +7,36 @@
 
 var temp = Number(prompt("What temperature is it?"));
 var unit = prompt("Is the temperature Celsius or Fahrenheit?", 
-	"Enter C for Celsius or F for Fahrenheit");
+    "Enter C for Celsius or F for Fahrenheit");
 var degCent;
 var degFahren;
 
 if(unit === "C"){
-	degCent = temp;
-	degFahren = 9/5 * degCent + 32;
-	console.log("The temperature is " + degFahren + " degrees Fahrenheit.");
+    degCent = temp;
+    degFahren = 9/5 * degCent + 32;
+    console.log("The temperature is " + degFahren + " degrees Fahrenheit.");
 }else{
-	degFahren = temp;
-	degCent = 5/9 * (degFahren - 32);
-	console.log("The temperature is " + degCent + " degrees Celsius.");
+    degFahren = temp;
+    degCent = 5/9 * (degFahren - 32);
+    console.log("The temperature is " + degCent + " degrees Celsius.");
 }
 
+// Last Chance for Gas!
+// ====================
+
+var gasEfficiency = Number(prompt("How efficient is the car?", 
+    "Enter the efficiency in miles per gallon (MPG)."));
+var percentageLeft = Number(prompt("How much of the tank is left?", 
+    "Enter a percentage from 0 to 100."));
+var tankCapacity = Number(prompt("How much total gas can the car hold?", 
+    "Enter the car's tank capacity in gallons."));
+
+var gasLeft = percentageLeft / 100 * tankCapacity;
+var milesLeft = gasEfficiency * gasLeft;
+
+if(milesLeft > 200){
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    console.log("You only have " + gasLeft 
+        + " gallons of gas in your tank, better get gas now while you can!");
+}
