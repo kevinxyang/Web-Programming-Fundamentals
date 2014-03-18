46a6cecf3880f8d4b9b6d13c38e85a6b1a5977f5
kevinxyang <kyang@fullsail.edu>
2014-03-18 13:47:20 -0400

    Finished code, and printed results out to the console.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,24 +11,35 @@
 // Givens
 // ------
 
-var currentWeight;
-var dailyExpenditure;
-var breakfastIntake;
-var lunchIntake;
-var dinnerIntake;
+var currentWeight = Number(prompt("Enter your current weight"));
+var dailyExpenditure = Number(prompt("Enter your total daily expenditure estimate"));
+var breakfastIntake = Number(prompt("Enter the number of calories you consumed for breakfast"));
+var lunchIntake = Number(prompt("Enter the number of calories you consumed for lunch"));
+var dinnerIntake = Number(prompt("Enter the number of calories you consumed for dinner"));
 
 
 
 // Calculations
 // ------------
 
-var weightChange;
-var percentageChange;
-var weightPrediction;
+var caloriesConsumed = breakfastIntake + lunchIntake + dinnerIntake;
+var weightChange = (caloriesConsumed - dailyExpenditure) / 3500;
+var weightPrediction = currentWeight + weightChange * 21;
 
 
 
 // Output
 // ------
 
-console.log("Hello World!");
+if(weightChange < 0){
+	console.log("You have lost " + weightChange + "lbs. Good job!");
+	console.log("If everyday was like today, you would weigh " + weightPrediction + "lbs in 3 weeks.");
+}else if(weightChange == 0){
+	console.log("You have lost " + weightChange + "lbs. Try a little harder tomorrow.");
+	console.log("If everyday was like today, you would weigh " + weightPrediction + "lbs in 3 weeks.");;
+}else{
+	console.log("You have gained " + weightChange + "lbs. Better get on it!");
+	console.log("If everyday was like today, you would weigh " + weightPrediction + "lbs in 3 weeks.");
+}
+
+// End
