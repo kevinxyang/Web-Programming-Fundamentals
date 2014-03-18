d1388a22ea42b5f0a57a408ae5878a4b85b98396
kevinxyang <kyang@fullsail.edu>
2014-03-18 13:22:18 -0400

    Laid out basic structure of the code.

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,34 @@
+// Kevin Yang
+// Conditionals - Personal
+// March 18th, 2014
+// =======================
+
+
+
+// Personal - Calorie Counter
+// ==========================
+
+// Givens
+// ------
+
+var currentWeight;
+var dailyExpenditure;
+var breakfastIntake;
+var lunchIntake;
+var dinnerIntake;
+
+
+
+// Calculations
+// ------------
+
+var weightChange;
+var percentageChange;
+var weightPrediction;
+
+
+
+// Output
+// ------
+
+console.log("Hello World!");
