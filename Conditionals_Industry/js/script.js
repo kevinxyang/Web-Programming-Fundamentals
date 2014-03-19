dedc9c97ecf22ee0f3010949b21961425e8a56aa
kevinxyang <kyang@fullsail.edu>
2014-03-19 17:30:04 -0400

    Wrote more code, still need validation.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -11,11 +11,13 @@
 // Givens
 // ------
 
-var month = prompt("Enter the month that corresponds to this data.");
+var month = prompt("Enter the month that corresponds to this data.", "January");
+
 var mobileWeek1 = prompt("Enter the number of people that viewed your site through a mobile device in week 1.");
 var mobileWeek2 = prompt("Enter the number of people that viewed your site through a mobile device in week 2.");
 var mobileWeek3 = prompt("Enter the number of people that viewed your site through a mobile device in week 3.");
 var mobileWeek4 = prompt("Enter the number of people that viewed your site through a mobile device in week 4.");
+
 var desktopWeek1 = prompt("Enter the number of people that viewed your site through a desktop device in week 1.");
 var desktopWeek2 = prompt("Enter the number of people that viewed your site through a desktop device in week 2.");
 var desktopWeek3 = prompt("Enter the number of people that viewed your site through a desktop device in week 3.");
@@ -43,9 +45,18 @@ siteTraffic[1][3] = Number(desktopWeek4);
 var totalMobile = siteTraffic[0][0] + siteTraffic[0][1] + siteTraffic[0][2] + siteTraffic[0][3];
 var totalDesktop = siteTraffic[1][0] + siteTraffic[1][1] + siteTraffic[1][2] + siteTraffic[1][3];
 var totalVisitors = totalMobile + totalDesktop;
-// Write a conditional (30 is temp.)
-var averageMobile = totalVisitors / 30;
-var averageDesktop = totalVisitors / 30;
+
+var days;
+if(month === "September" || month === "April" || month === "June" || month === "November"){
+    days = 30;
+}else if(month === "February"){
+    days = 28;
+}else{
+    days = 31;
+}
+
+var averageMobile = totalMobile / days;
+var averageDesktop = totalDesktop / days;
 var percentageMobile = totalMobile / totalVisitors;
 var percentageDesktop = totalDesktop / totalVisitors;
 
@@ -54,9 +65,10 @@ var percentageDesktop = totalDesktop / totalVisitors;
 // Output
 // ------
 
-console.log(totalVisitors);
-console.log(percentageMobile);
-console.log(percentageDesktop);
-// If mobile > desktop, print maybe consider a mobile first approach! Else keep doing what you're doing.
-// Make this a ternary operator.
-console.log("Hello World!");
+console.log("You had a total of " +totalVisitors + " visitors in the month of " + month + ".");
+console.log("An average of " + averageMobile + " people viewed your site through a mobile device per day.");
+console.log("An average of " + averageDesktop + " people viewed your site through a desktop device per day.");
+(percentageMobile > percentageDesktop) ? console.log((percentageMobile - percentageDesktop) * 100 
+    + "% more users viewed your site through a mobile device!") : 
+    console.log((percentageDesktop - percentageMobile) * 100 
+    + "% more users viewed your site through a desktop device!");
