db1a7866f12a2f462b8cdf7284837c0ded5592ac
kevinxyang <kyang@fullsail.edu>
2014-03-18 23:17:52 -0400

    Structured the code.

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,43 @@
+// Kevin Yang
+// Conditionals Assignment
+// March 19th, 2014
+// =======================
+
+
+
+// Industry - Mobile First or...?
+// ==============================
+
+// Givens
+// ------
+
+var month;
+var mobileWeek1;
+var mobileWeek2;
+var mobileWeek3;
+var mobileWeek4;
+var desktopWeek1;
+var desktopWeek2;
+var desktopWeek3;
+var desktopWeek4;
+var siteTraffic = Array();
+
+
+
+// Calculations
+// ------------
+
+var totalVisitors;
+var totalMobile;
+var totalDesktop;
+var averageMobile;
+var averageDesktop;
+var percentageMobile;
+var percentageDesktop;
+
+
+
+// Output
+// ------
+
+console.log("Hello World!");
