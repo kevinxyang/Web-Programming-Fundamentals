cb0d2e72c1f461f987bf0d80a95058b5390ca153
kevinxyang <kyang@fullsail.edu>
2014-03-19 23:00:29 -0400

    Set up files.

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,21 @@
+// Kevin Yang
+// Conditionals Assignment
+// March 18th, 2014
+// =======================
+
+
+
+// Givens
+// ------
+
+
+
+// Calculations
+// ------------
+
+
+
+// Output
+// ------
+
+console.log("Hello World!");
