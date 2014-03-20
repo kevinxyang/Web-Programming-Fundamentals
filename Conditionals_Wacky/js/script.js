24d7c692d7a2ae6578fbe15e17068fa43682949f
kevinxyang <kyang@fullsail.edu>
2014-03-20 13:03:30 -0400

    Wrote the print statments.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,25 +8,25 @@
 // Givens
 // ------
 
-var sneezes;
-var blessYous;
+var sneezes = prompt("Enter the number of times you've heard someone sneeze today.");
+var blessYous = prompt("Enter the number of times you've heard someone say bless you after a sneeze today.");
 
 
 
 // Calculations
 // ------------
 
-var ratio = Number(blessYous) / Number(sneezes);
+var ratio = Number(blessYous) / Number(sneezes) * 100;
 
 
 
 // Output
 // ------
 
-if(ratio > .8){
-	console.log(A);
-}else if(ratio > .5){
-	console.log(B);
+if(ratio > 80){
+	console.log("Today " + ratio + "% of sneezes were followed by bless yous, you have some courteous friends!");
+}else if(ratio > 50){
+	console.log("Today " + ratio + "% of sneezes were followed by bless yous, some went unnoticed.");
 }else{
-	console.log(C);
+	console.log("Today " + ratio + "% of sneezes were followed by bless yous, teach your friends some manners!");
 }
