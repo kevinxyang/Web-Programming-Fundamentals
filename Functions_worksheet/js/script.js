341253543a5ba73a9c0f7b8e3216535acde63453
kevinxyang <kyang@fullsail.edu>
2014-03-24 21:40:38 -0400

    Wrote code for circumference and bee stings.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -3,4 +3,74 @@
 // March 24, 2014
 // ===================
 
-console.log("Hello World!");
+
+
+// Circumference of a Circle
+// =========================
+
+// Givens
+// ------
+
+var radius = Number(prompt("What is the radius of the circle?"));
+
+
+
+// Functions
+// ------------
+
+var circumference = function(radius){
+	circumference = 2 * 3.14 * radius;
+
+	return circumference
+}
+
+
+
+// Calculations
+// ------------
+
+circumference(radius);
+
+
+
+// Output
+// ------
+
+console.log("The circumference of the circle is " + circumference + ".");
+
+
+
+// Stung!
+// ======
+
+// Givens
+// ------
+
+var weight = Number(prompt("What is the victim's weight in pounds?"));
+
+
+
+// Functions
+// ---------
+
+var numberOfStings = function(weight){
+	numberOfStings = 8.666666667 * weight;
+
+	return numberOfStings;
+}
+
+
+
+// Calculations
+// ------------
+
+numberOfStings(weight);
+
+
+
+// Output
+// ------
+
+console.log("It takes " + numberOfStings + " bee stings to kill this animal.");
+
+// End
