f2dc55a4d8d6d07c9a5641cbd27b28fbb66a6007
kevinxyang <kyang@fullsail.edu>
2014-03-26 17:10:59 -0400

    Fixed tabs.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -18,7 +18,7 @@ var games = Number(prompt("How many games would you like to play?", "Please ente
 
 // Checks to see that it is an odd number of games.
 if (games % 2 == 0){
-	games = Number(prompt("Please enter an ODD number."));
+    games = Number(prompt("Please enter an ODD number."));
 }
 
 // Creates an array that will hold all the users moves.
@@ -26,12 +26,12 @@ var gamesArray = Array();
 
 // For each game, prompt the user for his or her "move".
 for (var i = 0; i < games; i++){
-	gamesArray[i] = prompt("Rock, Paper, Scissors... SHOOT!", "Rock");
+    gamesArray[i] = prompt("Rock, Paper, Scissors... SHOOT!", "Rock");
 
-	// If it's not a valid move, reprompt the user.
-	if (gamesArray[i] != "Rock" && gamesArray[i] != "Paper" && gamesArray[i] != "Scissors"){
-		gamesArray[i] = prompt("Please enter a VALID value.", "Rock");
-	}
+    // If it's not a valid move, reprompt the user.
+    if (gamesArray[i] != "Rock" && gamesArray[i] != "Paper" && gamesArray[i] != "Scissors"){
+        gamesArray[i] = prompt("Please enter a VALID value.", "Rock");
+    }
 }
 
 
@@ -41,93 +41,93 @@ for (var i = 0; i < games; i++){
 
 // A function that randomly generates the computers moves.
 var computerMoves = function(){
-	// An array of "possible" computer moves.
-	var moves = Array();
-	moves[0] = "Rock";
-	moves[1] = "Paper";
-	moves[2] = "Scissors";
+    // An array of "possible" computer moves.
+    var moves = Array();
+    moves[0] = "Rock";
+    moves[1] = "Paper";
+    moves[2] = "Scissors";
 
-	// An array that holds the computers moves.
-	var movesChosen = Array();
+    // An array that holds the computers moves.
+    var movesChosen = Array();
 
-	// For each game, randomly choose a possible move for the computer.
-	for (var i = 0; i < games; i++){
-		movesChosen[i] = moves[Math.floor(Math.random() * moves.length)];
-	}
-	
-	// Returns the array of moves.
-	return movesChosen;
+    // For each game, randomly choose a possible move for the computer.
+    for (var i = 0; i < games; i++){
+        movesChosen[i] = moves[Math.floor(Math.random() * moves.length)];
+    }
+    
+    // Returns the array of moves.
+    return movesChosen;
 
 }
 
 // A function that "simulates" each game by comparing the player's move with the computer's move.
 var simulator = function(gamesArray, computerMoves){
-	// Declared variables for wins, losses, and ties. We will increment them. Default set to 0.
-	var wins = 0;
-	var losses = 0;
-	var ties = 0;
-	// An array that will eventually hold the above variables, after we've incremented them.
-	var resultsArray = Array();
+    // Declared variables for wins, losses, and ties. We will increment them. Default set to 0.
+    var wins = 0;
+    var losses = 0;
+    var ties = 0;
+    // An array that will eventually hold the above variables, after we've incremented them.
+    var resultsArray = Array();
 
-	// For each game, compare the moves.
-	for (var i = 0; i < games; i++){
-		console.log("------------ " + "Game " + (i + 1) + " ------------");
-		// If the player's move and the computer's move are the same, then increment ties by 1.
-		if (gamesArray[i] == movesChosen[i]){
-			console.log("You both chose " + gamesArray[i] + ", it's a tie!");
-			ties++;
-		}
-		// Else if, the player chooses Rock...
-		else if (gamesArray[i] == "Rock"){
-			// and the computer chooses Scissors, then increment wins by 1.
-			if (movesChosen[i] == "Scissors"){
-				console.log("Rock beats Scissors, you win!");
-				wins++;
-			// else, increment losses by 1.
-			} else {
-				console.log("Rock loses to Paper, you lose!");
-				losses++;
-			}
-		}
-		// Else if, the player chooses Paper...
-		else if (gamesArray[i] == "Paper"){
-			// and the computer chooses Rock, increment wins by 1.
-			if (movesChosen[i] == "Rock"){
-				console.log("Paper beats Rock, you win!");
-				wins++;
-			// else, increment losses by 1.
-			} else {
-				console.log("Paper loses to Scissors, you lose!");
-				losses++;
-			}
-		}
-		// Else if, the player choose Scissors...
-		else if (gamesArray[i] == "Scissors"){
-			// and the computer chooses Paper, increment wins by 1.
-			if (movesChosen[i] == "Paper"){
-				console.log("Scissors beats Paper, you win!");
-				wins++;
-			// else, increment losses by 1.
-			} else {
-				console.log("Scissors loses to Rock, you lose!");
-				losses++;
-			}
-		}
-	}
-	// Print the totals to console.
-	console.log(" ");
-	console.log("-----------------");
-	console.log("Total Wins: " + wins);
-	console.log("Total Losses: " + losses);
-	console.log("Total Ties: " + ties);
+    // For each game, compare the moves.
+    for (var i = 0; i < games; i++){
+        console.log("------------ " + "Game " + (i + 1) + " ------------");
+        // If the player's move and the computer's move are the same, then increment ties by 1.
+        if (gamesArray[i] == movesChosen[i]){
+            console.log("You both chose " + gamesArray[i] + ", it's a tie!");
+            ties++;
+        }
+        // Else if, the player chooses Rock...
+        else if (gamesArray[i] == "Rock"){
+            // and the computer chooses Scissors, then increment wins by 1.
+            if (movesChosen[i] == "Scissors"){
+                console.log("Rock beats Scissors, you win!");
+                wins++;
+            // else, increment losses by 1.
+            } else {
+                console.log("Rock loses to Paper, you lose!");
+                losses++;
+            }
+        }
+        // Else if, the player chooses Paper...
+        else if (gamesArray[i] == "Paper"){
+            // and the computer chooses Rock, increment wins by 1.
+            if (movesChosen[i] == "Rock"){
+                console.log("Paper beats Rock, you win!");
+                wins++;
+            // else, increment losses by 1.
+            } else {
+                console.log("Paper loses to Scissors, you lose!");
+                losses++;
+            }
+        }
+        // Else if, the player choose Scissors...
+        else if (gamesArray[i] == "Scissors"){
+            // and the computer chooses Paper, increment wins by 1.
+            if (movesChosen[i] == "Paper"){
+                console.log("Scissors beats Paper, you win!");
+                wins++;
+            // else, increment losses by 1.
+            } else {
+                console.log("Scissors loses to Rock, you lose!");
+                losses++;
+            }
+        }
+    }
+    // Print the totals to console.
+    console.log(" ");
+    console.log("-----------------");
+    console.log("Total Wins: " + wins);
+    console.log("Total Losses: " + losses);
+    console.log("Total Ties: " + ties);
 
-	// Add results to the results array.
-	resultsArray[0] = wins;
-	resultsArray[1] = losses;
-	resultsArray[2] = ties;
+    // Add results to the results array.
+    resultsArray[0] = wins;
+    resultsArray[1] = losses;
+    resultsArray[2] = ties;
 
-	// Return the results array.
-	return resultsArray;
+    // Return the results array.
+    return resultsArray;
 
 }
 
