a7a4775e4fb6cb0dacc431d6e78d04c9fa82cce7
kevinxyang <kyang@fullsail.edu>
2014-03-25 23:31:34 -0400

    Wrote functions for total time and longest time.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,28 +5,64 @@
 
 
 
-// Title Here
-// ==========
+// Website Activity Tracker
+// ========================
 
 
 
 // Givens
 // ------
 
+var numberOfSites = Number(prompt("How many sites have you visited today?")) - 1;
+var siteList = Array();
+
+for (numberOfSites; numberOfSites >= 0; numberOfSites--){
+	siteList[numberOfSites] = Array();
+	siteList[numberOfSites][0] = prompt("Enter the site name");
+	siteList[numberOfSites][1] = Number(prompt("Enter the amount of time in minutes you spent on this site."));
+}
+
 
 
 // Functions
 // ---------
+
+function totalTime(){
+	var time = 0;
+	var arrayLength = siteList.length;
+	for (var i = 0; i < arrayLength; i++){
+		time += siteList[i][1];
+	}
+	return time;
+}
+
+function longestTime(){
+	var site = "";
+	var time = 0;
+	var arrayLength = siteList.length;
+	for (var i = 0; i < arrayLength; i++){
+		if (siteList[i][1] > time){
+			site = siteList[i][0];
+			time = siteList[i][1];
+		}
+	}
+	return [site, time];
+}
 
 
 
 // Calculations
 // ------------
 
+var timeSpent = totalTime();
+var mostTime = longestTime();
+
 
 
 // Output
 // ------
-console.log("Hello World!");
+console.log(siteList);
+console.log(timeSpent);
+console.log(mostTime);
 
 
